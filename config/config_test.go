49a4a6598b350566341483414e82384b81b02cc0
Michal Jarco <michal.jarco@allegrogroup.com>
2016-11-03 12:12:37 +0100

    YAMLURL tests

--- config/config_test.go
+++ config/config_test.go
@@ -10,17 +10,32 @@ type TestYaml struct {
 	Field YAMLURL
 }
 
-func TestYAMLURLParsing(t *testing.T) {
+func TestYAMLURLParsingSuccessful(t *testing.T) {
 	correct := []byte(`field: http://golang.org:80/pkg/net`)
 	testyaml := TestYaml{}
 	err := yaml.Unmarshal(correct, &testyaml)
 	if err != nil {
 		t.Error(err.Error())
 	}
+}
+
+func TestYAMLURLParsingFailure(t *testing.T) {
 	incorrect := []byte(`field: golang.org:80/pkg/net`)
-	testyaml2 := TestYaml{}
-	err = yaml.Unmarshal(incorrect, &testyaml2)
+	testyaml := TestYaml{}
+	err := yaml.Unmarshal(incorrect, &testyaml)
 	if err == nil {
 		t.Errorf("Missing protocol should return error")
+	}
+}
+
+func TestYAMLURLParsingEmpty(t *testing.T) {
+	incorrect := []byte(`field:`)
+	testyaml := TestYaml{}
+	err := yaml.Unmarshal(incorrect, &testyaml)
+	if err != nil {
+		t.Errorf("Should not even try to parse")
+	}
+	if testyaml.Field.URL != nil {
+		t.Errorf("Should be nil")
 	}
 }
