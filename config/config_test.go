3f0207911dc9f615c21c5b4724d5500f7dc5f509
Michal Jarco <michal.jarco@allegrogroup.com>
2016-11-04 12:36:05 +0100

    Use Error/NoError instead Nil/NotNil

--- config/config_test.go
+++ config/config_test.go
@@ -15,20 +15,20 @@ func TestYAMLURLParsingSuccessful(t *testing.T) {
 	correct := []byte(`field: http://golang.org:80/pkg/net`)
 	testyaml := TestYaml{}
 	err := yaml.Unmarshal(correct, &testyaml)
-	assert.Nil(t, err, "Should be correct")
+	assert.NoError(t, err, "Should be correct")
 }
 
 func TestYAMLURLParsingFailure(t *testing.T) {
 	incorrect := []byte(`field: golang.org:80/pkg/net`)
 	testyaml := TestYaml{}
 	err := yaml.Unmarshal(incorrect, &testyaml)
-	assert.NotNil(t, err, "Missing protocol should return error")
+	assert.Error(t, err, "Missing protocol should return error")
 }
 
 func TestYAMLURLParsingEmpty(t *testing.T) {
 	incorrect := []byte(`field:`)
 	testyaml := TestYaml{}
 	err := yaml.Unmarshal(incorrect, &testyaml)
-	assert.Nil(t, err, "Should not even try to parse")
+	assert.NoError(t, err, "Should not even try to parse")
 	assert.Nil(t, testyaml.Field.URL, "Should be nil")
 }
