de17869835176e2b04bd47472a8573ebbee35a2b
Marcin Bielak <marcin.bieli@gmail.com>
2017-11-07 21:33:18 +0100

    adding unit tests for types - CI fixing

---
+++ types/yaml_test.go
@@ -0,0 +1,38 @@
+package types
+
+import (
+	"testing"
+
+	"net/url"
+
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+)
+
+// TestYaml type for tests
+type TestYaml struct {
+	URL YAMLUrl `yaml:"URL"`
+}
+
+// unmareshal func for tests
+var unmareshal = func(interface{}) error {
+	var err error
+	return err
+}
+
+func TestYAMLUrlParsingFailure(t *testing.T) {
+	testyaml := TestYaml{URL: YAMLUrl{&url.URL{}}}
+	err := testyaml.URL.UnmarshalYAML(unmareshal)
+	assert.Equal(t, err.Error(), "url should match proto://host[:port]/path scheme - got \"\"")
+}
+
+func TestYAMLUrlParsingSuccessful(t *testing.T) {
+	expectedUrl := "http://golang.org:80/pkg/net"
+	correct := []byte(`URL: ` + expectedUrl)
+	testyaml := TestYaml{URL: YAMLUrl{}}
+	err := yaml.Unmarshal(correct, &testyaml)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedUrl, fmt.Sprintf("%s", testyaml.URL))
+}
