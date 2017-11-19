1e0b4f82463b15d18a894c21ee45dbb187dae7ae
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-11-19 20:42:44 +0100

    fix: preallocate maps

--- sharding/sharding.go
+++ sharding/sharding.go
@@ -22,14 +22,14 @@ type RingFactory struct {
 }
 
 func (rf RingFactory) uniqBackends(regionCfg shardingconfig.RegionConfig) ([]url.URL, error) {
-	allBackendsSet := make(map[shardingconfig.YAMLUrl]bool)
+	allBackendsSet := make(map[shardingconfig.YAMLUrl]struct{})
 	for _, clusterConfig := range regionCfg.Clusters {
 		clientCluster, err := rf.storages.GetCluster(clusterConfig.Cluster)
 		if err != nil {
 			return nil, err
 		}
 		for _, backendURL := range clientCluster.Backends {
-			allBackendsSet[backendURL] = true
+			allBackendsSet[backendURL] = struct{}{}
 		}
 	}
 	var uniqBackendsSlice []url.URL
@@ -40,7 +40,7 @@ func (rf RingFactory) uniqBackends(regionCfg shardingconfig.RegionConfig) ([]url
 }
 
 func (rf RingFactory) getRegionClusters(regionCfg shardingconfig.RegionConfig) map[string]int {
-	res := make(map[string]int)
+	res := make(map[string]int, len(regionCfg.Clusters))
 	for _, clusterConfig := range regionCfg.Clusters {
 		res[clusterConfig.Cluster] = int(math.Floor(clusterConfig.Weight * 100))
 	}
@@ -60,7 +60,7 @@ func (rf RingFactory) makeClusterMap(clientClusters map[string]int) (map[string]
 }
 
 func (rf RingFactory) createRegressionMap(regionConfig shardingconfig.RegionConfig) (map[string]storages.Cluster, error) {
-	regressionMap := make(map[string]storages.Cluster)
+	regressionMap := make(map[string]storages.Cluster, len(regionConfig.Clusters))
 	var previousCluster storages.Cluster
 	for i, cluster := range regionConfig.Clusters {
 		clientCluster, err := rf.storages.GetCluster(cluster.Cluster)
@@ -90,15 +90,15 @@ func (rf RingFactory) RegionRing(regionCfg shardingconfig.RegionConfig) (ShardsR
 
 	respHandler := httphandler.LateResponseHandler(rf.conf)
 
+	regressionMap, err := rf.createRegressionMap(regionCfg)
+	if err != nil {
+		return ShardsRing{}, nil
+	}
 	allBackendsRoundTripper := transport.NewMultiTransport(
 		rf.transport,
 		allBackendsSlice,
 		respHandler,
 		rf.conf.MaintainedBackends)
-	regressionMap, err := rf.createRegressionMap(regionCfg)
-	if err != nil {
-		return ShardsRing{}, nil
-	}
 	return ShardsRing{
 		cHashMap,
 		shardClusterMap,
