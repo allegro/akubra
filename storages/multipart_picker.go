4264f4e36a4e8bf7f84d9067fff4a76e254cf513
Lukasz Bogaczynski <lukasz.bogaczynski@allegro.pl>
2018-02-13 08:41:59 +0100

    Pick the first backend from list for multi part upload instead of randomly picking one.

--- storages/multipart_picker.go
+++ storages/multipart_picker.go
@@ -2,9 +2,6 @@ package storages
 
 import (
 	"net/http"
-	"math/rand"
-	"time"
-	"github.com/allegro/akubra/log"
 )
 
 func PickRandomBackendForMultiPartUpload(backends []http.RoundTripper) (multiUploadBackend *Backend, backendsHostNamesToSync []string) {
@@ -13,35 +10,15 @@ func PickRandomBackendForMultiPartUpload(backends []http.RoundTripper) (multiUpl
 		return nil, []string{}
 	}
 
-	rand.Seed(time.Now().Unix())
+	for _, backend := range backends {
 
-	var multiPartUploadBackendIndex int
-	alreadyPickedBackendsIndexes := make(map[int]interface{})
+		if backend, isBackendType := backend.(*Backend); isBackendType {
 
-	for {
-
-		if len(alreadyPickedBackendsIndexes) >= len(backends) {
-			log.Debugf("Couldn't pick a backend for multipart upload from %s, all seem to be in Maintenance mode.", backends)
-			return nil, []string{}
-		}
-
-		randomBackendIndex := rand.Intn(len(backends))
-		randomMultiUploadBackend, isBackendType := backends[randomBackendIndex].(*Backend)
-
-		if isBackendType && !randomMultiUploadBackend.Maintenance {
-			multiPartUploadBackendIndex = randomBackendIndex
-			multiUploadBackend = randomMultiUploadBackend
-			break
-		}
-
-		if _, alreadyPicked := alreadyPickedBackendsIndexes[randomBackendIndex]; !alreadyPicked {
-			alreadyPickedBackendsIndexes[randomBackendIndex] = nil
-		}
-	}
-
-	for index, backend := range backends {
-		if backend, isBackendType := backend.(*Backend); isBackendType && index != multiPartUploadBackendIndex {
-			backendsHostNamesToSync = append(backendsHostNamesToSync, backend.Name)
+			if multiUploadBackend == nil && !backend.Maintenance {
+				multiUploadBackend = backend
+			} else {
+				backendsHostNamesToSync = append(backendsHostNamesToSync, backend.Name)
+			}
 		}
 	}
 
--- storages/multipart_picker_test.go
+++ storages/multipart_picker_test.go
@@ -9,23 +9,7 @@ import (
 
 func TestShouldReturnNilBackendAndEmptyHostnameListWhenProvidedBackendListIsEmpty(testSuite *testing.T) {
 
-
 	backendPickedForMultiPartUpload, hostnamesToSync := PickRandomBackendForMultiPartUpload([]http.RoundTripper{})
-
-	assert.Nil(testSuite, backendPickedForMultiPartUpload)
-	assert.Empty(testSuite, hostnamesToSync)
-}
-
-func TestShouldReturnNilBackendAndEmptyHostnameWhenAllBackendsAreInMaintenanceMode(testSuite *testing.T) {
-
-	backend := &Backend{
-		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: true,
-		Name : "someBackend",
-	}
-
-	backendPickedForMultiPartUpload, hostnamesToSync := PickRandomBackendForMultiPartUpload([]http.RoundTripper{backend})
 
 	assert.Nil(testSuite, backendPickedForMultiPartUpload)
 	assert.Empty(testSuite, hostnamesToSync)
@@ -35,9 +19,9 @@ func TestShouldReturnMultiPartUploadBackendAndEmptyHostnamesToSyncListWhenOnlyOn
 
 	backend := &Backend{
 		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: false,
-		Name : "someBackend",
+		Endpoint:     url.URL{},
+		Maintenance:  false,
+		Name:         "someBackend",
 	}
 
 	backendPickedForMultiPartUpload, hostnamesToSync := PickRandomBackendForMultiPartUpload([]http.RoundTripper{backend})
@@ -50,62 +34,51 @@ func TestShouldReturnMultiPartUploadBackendAndOneBackendHostnameToSync(testSuite
 
 	backend1 := &Backend{
 		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: false,
-		Name : "someBackend1",
+		Endpoint:     url.URL{},
+		Maintenance:  false,
+		Name:         "someBackend1",
 	}
 
 	backend2 := &Backend{
 		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: false,
-		Name : "someBackend2",
+		Endpoint:     url.URL{},
+		Maintenance:  false,
+		Name:         "someBackend2",
 	}
-
 
 	backendPickedForMultiPartUpload, hostnamesToSync := PickRandomBackendForMultiPartUpload([]http.RoundTripper{backend1, backend2})
 
+	assert.Equal(testSuite, backendPickedForMultiPartUpload, backend1)
 	assert.Equal(testSuite, len(hostnamesToSync), 1)
-
-	if hostnamesToSync[0] == "someBackend1" {
-		assert.Equal(testSuite, backendPickedForMultiPartUpload, backend2)
-	} else {
-		assert.Equal(testSuite, backendPickedForMultiPartUpload, backend1)
-	}
+	assert.Equal(testSuite, hostnamesToSync[0], "someBackend2")
 }
 
 func TestShouldReturnMultiPartUploadBackendAndManyBackendsHostnamesToSync(testSuite *testing.T) {
 
 	backend1 := &Backend{
 		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: false,
-		Name : "someBackend1",
+		Endpoint:     url.URL{},
+		Maintenance:  false,
+		Name:         "someBackend1",
 	}
 
 	backend2 := &Backend{
 		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: false,
-		Name : "someBackend2",
+		Endpoint:     url.URL{},
+		Maintenance:  false,
+		Name:         "someBackend2",
 	}
 
 	backend3 := &Backend{
 		RoundTripper: nil,
-		Endpoint: url.URL{},
-		Maintenance: false,
-		Name : "someBackend2",
+		Endpoint:     url.URL{},
+		Maintenance:  false,
+		Name:         "someBackend2",
 	}
 
 	backendPickedForMultiPartUpload, hostnamesToSync := PickRandomBackendForMultiPartUpload([]http.RoundTripper{backend1, backend2, backend3})
 
+	assert.Equal(testSuite, backendPickedForMultiPartUpload, backend1)
 	assert.Equal(testSuite, len(hostnamesToSync), 2)
-
-	if backendPickedForMultiPartUpload == backend1 {
-		assert.Contains(testSuite, hostnamesToSync, "someBackend2", "someBackend3")
-	} else if backendPickedForMultiPartUpload == backend2 {
-		assert.Contains(testSuite, hostnamesToSync, "someBackend1", "someBackend3")
-	} else {
-		assert.Contains(testSuite, hostnamesToSync, "someBackend1", "someBackend2")
-	}
+	assert.Contains(testSuite, hostnamesToSync, "someBackend2", "someBackend3")
 }
