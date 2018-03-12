b9c938994c01849f6727008ad5665f8f9310462c
Michał Jarco <michal.jarco@allegro.pl>
2018-03-12 16:59:06 +0100

    Added s3 datatypes for /?versions and /?uploads requests

--- storages/s3_datatypes_minio.go
+++ storages/s3_datatypes_minio.go
@@ -70,3 +70,127 @@ type ListBucketResult struct {
 	NextMarker string
 	Prefix     string
 }
+
+// OwnerInfo owner info container
+type UserInfo struct {
+	ID          string
+	DisplayName string
+}
+
+// VersionInfo version item container
+type VersionInfo struct {
+	Key          string
+	VersionID    string `xml:"VersionId"`
+	IsLatest     bool
+	LastModified time.Time
+	ETag         string
+	Size         int64
+	StorageClass string
+	Owner        UserInfo
+}
+
+// DeleteMarkerInfo container
+type DeleteMarkerInfo struct {
+	Key          string
+	VersionID    string `xml:"VersionId"`
+	IsLatest     bool
+	LastModified time.Time
+	Owner        UserInfo
+}
+
+// ListVersionsResult container for Bucket Object versions response
+// see: https://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGETVersion.html
+type ListVersionsResult struct {
+	Name            string
+	Prefix          string
+	KeyMarker       string
+	VersionIDMarker string
+	MaxKeys         int64
+	EncodingType    string
+
+	// A flag that indicates whether or not ListObjects returned all of the results
+	// that satisfied the search criteria.
+	IsTruncated  bool
+	Version      []VersionInfo
+	DeleteMarker []DeleteMarkerInfo
+	// When response is truncated (the IsTruncated element value in
+	// the response is true), you can use the key name in this field
+	// as marker in the subsequent request to get next set of objects.
+	// Object storage lists objects in alphabetical order Note: This
+	// element is returned only if you have delimiter request
+	// parameter specified. If response does not include the NextMaker
+	// and it is truncated, you can use the value of the last Key in
+	// the response as the marker in the subsequent request to get the
+	// next set of object keys.
+	NextKeyMarker       string
+	NextVersionIDMarker string `xml:"NextVersionIdMarker"`
+}
+
+// ListBucketV2Result container for listObjects response version 2.
+type ListBucketV2Result struct {
+	// A response can contain CommonPrefixes only if you have
+	// specified a delimiter.
+	CommonPrefixes []CommonPrefix
+	// Metadata about each object returned.
+	Contents  []ObjectInfo
+	Delimiter string
+
+	// Encoding type used to encode object keys in the response.
+	EncodingType string
+
+	// A flag that indicates whether or not ListObjects returned all of the results
+	// that satisfied the search criteria.
+	IsTruncated bool
+	MaxKeys     int64
+	Name        string
+
+	// Hold the token that will be sent in the next request to fetch the next group of keys
+	NextContinuationToken string
+
+	ContinuationToken string
+	Prefix            string
+
+	// FetchOwner and StartAfter are currently not used
+	FetchOwner string
+	StartAfter string
+}
+
+type ListMultipartUploadsResult struct {
+	Bucket             string
+	KeyMarker          string
+	UploadIDMarker     string `xml:"UploadIdMarker"`
+	NextKeyMarker      string
+	NextUploadIDMarker string `xml:"NextUploadIdMarker"`
+	EncodingType       string
+	MaxUploads         int64
+	IsTruncated        bool
+	Uploads            []ObjectMultipartInfo `xml:"Upload"`
+	Prefix             string
+	Delimiter          string
+	// A response can contain CommonPrefixes only if you specify a delimiter.
+	CommonPrefixes []CommonPrefix
+}
+
+// ObjectMultipartInfo container for multipart object metadata.
+type ObjectMultipartInfo struct {
+	// Date and time at which the multipart upload was initiated.
+	Initiated time.Time `type:"timestamp" timestampFormat:"iso8601"`
+
+	Initiator UserInfo
+	Owner     UserInfo
+
+	// The type of storage to use for the object. Defaults to 'STANDARD'.
+	StorageClass string
+
+	// Key of the object for which the multipart upload was initiated.
+	Key string
+
+	// Size in bytes of the object.
+	Size int64
+
+	// Upload ID that identifies the multipart upload.
+	UploadID string `xml:"UploadId"`
+
+	// Error
+	Err error
+}
