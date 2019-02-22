2167a78f3b7f3460a2ed2d0e7ab68888274291d1
Lukasz Bogaczynski <lukasz.bogaczynski@allegro.pl>
2019-02-22 08:21:45 +0100

    Rename error variable

--- storages/backend/backend.go
+++ storages/backend/backend.go
@@ -42,9 +42,9 @@ func (b *Backend) RoundTrip(req *http.Request) (resp *http.Response, err error) 
 		err = &types.BackendError{HostName: b.Endpoint.Host, OrigErr: oerror}
 	} else if resp != nil {
 		if resp.Body != nil {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil  {
-				return nil, err
+			body, berr := ioutil.ReadAll(resp.Body)
+			if berr != nil  {
+				return nil, berr
 			}
 			log.Debugf("response for req %s: %s", reqID, string(body))
 			resp.Body = ioutil.NopCloser(bytes.NewReader(body))
