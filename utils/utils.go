add32bda01dbb01a363e758bcc0002071a8b40ea
Lukasz Bogaczynski <lukasz.bogaczynski@allegro.pl>
2019-04-09 16:31:16 +0200

    Trim path before checking

--- utils/utils.go
+++ utils/utils.go
@@ -79,7 +79,7 @@ func IsBucketPath(path string) bool {
 //IsObjectPath check if a given path is an object path
 func IsObjectPath(path string) bool {
 	//TODO add support for domain style paths when the domain support is going to be merged
-	parts := strings.SplitN(path, "/", 2)
+	parts := strings.SplitN(strings.Trim(path, "/"), "/", 2)
 	return len(parts) == 2 && parts[1] != ""
 }
 
